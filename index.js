afa38b70693d9bedf12aa3ff5f82540edf60902d
Alexon <alexchc89@gmail.com>
2021-03-18 02:26:39 -0700

    Done with assignment js 2 objects lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name: 'alex', streetAddress: '31 capri'}
+
+function updateEmployeeWithKeyAndValue(employee, name, value = 'Sam') {
+    const newGuy = {...employee};
+    newGuy[name] = value;
+    return newGuy
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value = '12 Broadway') {
+    employee[streetAddress] = value;
+    return employee 
+}
+function deleteFromEmployeeByKey (employee, name = 'Carl') {
+    const newGuy2 = {...employee};
+    delete newGuy2[name];
+    //or delete newGuy2.name;
+    return newGuy2
+}
+function destructivelyDeleteFromEmployeeByKey (employee, name) {
+    let newEmployee = employee;
+    delete employee[name]
+     return newEmployee
+}
